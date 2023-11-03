e30cc9b49c20c5097b86e83ca4aa36073ad03835
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-11-03 18:30:41 +0100

    fix: ignore .project, .classpath and .settings files

--- PizzaToGo/services/production/src/main/java/app/api/ProductionController.java
+++ PizzaToGo/services/production/src/main/java/app/api/ProductionController.java
@@ -1,11 +1,10 @@
-package procurement.client;
+package app.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
