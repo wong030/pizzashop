908f47f8ba407c9c6816f9c55e108f48ee21776f
JanLudwig <jalu1002@stud.hs-kl.de>
2023-11-15 09:13:06 +0100

    feat: add comments to the code

--- PizzaToGo/services/user/src/main/java/app/util/PasswordHelper.java
+++ PizzaToGo/services/user/src/main/java/app/util/PasswordHelper.java
@@ -1,5 +1,6 @@
 package app.util;
 
+//Imports für die Passwort Verschlüsselung
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -11,13 +12,20 @@ import javax.crypto.spec.PBEKeySpec;
 
 public class PasswordHelper {
 	
+	//this String defines which algorithm is used to hash the password (PBKDF2 Algorithm with HMAC-SHA-1 as pseudorandom function)
 	private static final String SECRET_KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA1";
+	//length of the key
 	private static final int KEY_LENGTH_BITS = 160;
+	//number of iterations for the Hash-Algorithm
 	private static final int ITERATION_COUNT = 65536;
 	
+	// pseudorandom number generator
 	private static final String RANDOM_NUMBER_GENERATOR_ALGORITHM = "SHA1PRNG";
+	//length of the password-salt
 	private static final int SALT_LENGTH_BYTES = 32;
 
+	
+	//function to generate the password-hash.
 	public static byte[] generatePasswordHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException{
 		final SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY_ALGORITHM);
 	    final KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, ITERATION_COUNT, KEY_LENGTH_BITS);
@@ -26,7 +34,8 @@ public class PasswordHelper {
 
 	    return secretKey.getEncoded();
 	  }
-
+	
+	//function to generate the password-salt
 	public static byte[] generateSalt() throws NoSuchAlgorithmException{
 	    final SecureRandom random = SecureRandom.getInstance(RANDOM_NUMBER_GENERATOR_ALGORITHM);
 	    final byte[] salt = new byte[SALT_LENGTH_BYTES];
