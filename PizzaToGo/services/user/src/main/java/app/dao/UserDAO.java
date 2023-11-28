e55af4c702d298263fcc718f5fce4dd0b473c3aa
JanLudwig <jalu1002@stud.hs-kl.de>
2023-11-28 20:27:11 +0100

    feat: implement UserDAO with method createUser()

--- PizzaToGo/services/user/src/main/java/app/dao/UserDAO.java
+++ PizzaToGo/services/user/src/main/java/app/dao/UserDAO.java
@@ -4,5 +4,8 @@ import app.model.User;
 import app.api.dto.RegistrationData;
 
 public interface UserDAO {
-
+	
+	User createUser(RegistrationData data);
+	
+	User readUser(int id);
 }
--- PizzaToGo/services/user/src/main/java/app/dao/UserDAOImpl.java
+++ PizzaToGo/services/user/src/main/java/app/dao/UserDAOImpl.java
@@ -1,8 +1,63 @@
 package app.dao;
 
 import app.model.User;
+
 import app.api.dto.RegistrationData;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import app.util.PasswordHelper;
+
 public class UserDAOImpl implements UserDAO {
+	
+	@PersistenceContext(name = "jpa-unit")
+	private EntityManager em;
+
+	@Override
+	public User createUser(RegistrationData data) {
+		
+		User user = new User();
+		user.setCity(data.getCity());
+		user.setEmail(data.getEmail());
+		user.setFirstName(data.getFirstName());
+		user.setLastName(data.getLastName());
+		user.setStreet(data.getStreet());
+		user.setStreetNr(data.getStreetNr());
+		user.setUserName(data.getUserName());
+		user.setZip(data.getZip());
+		
+		byte[] salt;
+		byte[] passwordHash;
+		
+		try {
+			salt = PasswordHelper.generateSalt();
+			passwordHash = PasswordHelper.generatePasswordHash(data.getPassword(), salt);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR User creation failed: " + e.getMessage());
+		}
+		
+		user.setPasswordSalt(salt);
+		user.setPasswordHash(passwordHash);
+		
+		em.persist(user);
+		em.flush();
+		em.refresh(user);
+		
+		return null;
+	}
+
+	@Override
+	public User readUser(int id) {
+		
+		return null;
+	}
+	
+	
 
 }
