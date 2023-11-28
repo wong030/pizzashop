739127ffaa879efcc391f5ae3c311f646e5358dc
JanLudwig <jalu1002@stud.hs-kl.de>
2023-11-28 20:16:36 +0100

    feat: implement class RegistrationData

--- PizzaToGo/services/user/src/main/java/app/api/dto/RegistrationData.java
+++ PizzaToGo/services/user/src/main/java/app/api/dto/RegistrationData.java
@@ -2,4 +2,69 @@ package app.api.dto;
 
 public class RegistrationData {
 	
+	private String userName;
+	private String password;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String street;
+	private String streetNr;
+	private String zip;
+	private String city;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getStreetNr() {
+		return streetNr;
+	}
+	public void setStreetNr(String streetNr) {
+		this.streetNr = streetNr;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
 }
