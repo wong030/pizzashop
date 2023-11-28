41f2eb349b3fb183bd2b81aed4b7670224b52cc4
JanLudwig <jalu1002@stud.hs-kl.de>
2023-11-28 20:34:28 +0100

    feat: implement UserController Class with POST-method registerUser

--- PizzaToGo/services/user/src/main/java/app/api/UserController.java
+++ PizzaToGo/services/user/src/main/java/app/api/UserController.java
@@ -1,18 +1,35 @@
 package app.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 import app.model.User;
 import app.dao.UserDAO;
 import app.api.dto.RegistrationData;
 
-@Path("/user")
+@Path("user")
 public class UserController {
+	
+	@Inject
+	private UserDAO userDAO;
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response registerUser(RegistrationData registrationData) {
+		
+		System.out.println();
+		System.out.println("Register " + registrationData );
+		
+		final User createdUser = userDAO.createUser(registrationData);
+		
+	return Response.ok().entity(createdUser).build();
+	}
 
+	
     @GET
     @Path("/{parameter}")
     @Produces(MediaType.APPLICATION_JSON)
