f033528cfe474e41480ba06be0e717b74f2b8271
wong <130790711+wong030@users.noreply.github.com>
2023-11-28 02:44:46 +0100

    feat: validate authentification shop

--- PizzaToGo/services/shop/src/main/java/app/api/ShopController.java
+++ PizzaToGo/services/shop/src/main/java/app/api/ShopController.java
@@ -1,13 +1,23 @@
 package app.api;
 
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import app.services.AuthenticationService;
+
 @Path("/shop")
 public class ShopController {
+
+    @Inject
+    private AuthenticationService authenticationService;
 
     @GET
     @Path("/{parameter}")
@@ -15,4 +25,29 @@ public class ShopController {
     public String doSomething(@PathParam("parameter") String parameter) {
         return String.format("Processed parameter value '%s'", parameter);
     }
+
+    @GET
+    @Path("/valid")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean validateAuthentication(String username, String password) throws IOException, InterruptedException{
+        
+        boolean response = authenticationService.authenticateUser(username, password);
+        return response;
+    }
+
+
+    /* 
+    @Path("/order")
+    public String placeOrder(){
+
+        return "x";
+    }
+
+
+    public String configurePizza(){
+
+
+    }
+    */
 }
---
+++ PizzaToGo/services/shop/src/main/java/app/services/AuthenticationService.java
@@ -0,0 +1,58 @@
+package app.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Singleton;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Singleton
+public class AuthenticationService {
+    
+    public Boolean authenticateUser(String username, String password) throws IOException, InterruptedException{
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Map für die Benutzerdaten
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("username", username);
+        userData.put("password", password);
+
+        // Konvertieren in JSON-String
+        String jsonBody = toJsonString(userData);
+
+        URI uri = URI.create("http://localhost:9082/authenticate");
+
+        // HTTP-POST-Request mit JSON-Body
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+
+        // Send Request und erhalte Response
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Response Code: " + response.statusCode());
+        System.out.println("Response Body: " + response.body());
+
+        // Response-Body als boolean-Wert
+        return Boolean.parseBoolean(response.body());
+    }
+
+    // Hilfsmethode
+    private String toJsonString(Object object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Fehler beim Konvertieren in JSON", e);
+        }
+    }
+}
