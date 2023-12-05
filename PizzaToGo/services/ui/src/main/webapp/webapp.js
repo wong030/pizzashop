885283d973f41e047ebdfe6e1f4bc715f8e70aea
Jakob <jakob.dressler@yahoo.de>
2023-12-05 13:19:38 +0100

    little adjustments

--- PizzaToGo/services/ui/src/main/webapp/webapp.js
+++ PizzaToGo/services/ui/src/main/webapp/webapp.js
@@ -2,13 +2,19 @@
  * 
  */
 
-let Pizzaliste = ["Margherita", "Prosciutto", "Diavolo", "Tuna"];
+let PIZZALIST = ["Margherita", "Prosciutto", "Diavolo", "Fungi"];
+let TOPPINGS = {
+	'Margherita': 'Cheese, Tomato Sauce',
+	'Prosciutto': 'Cheese, Tomato Sauce, Ham',
+	'Diavolo': 'Cheese, Tomato Sauce, Pepperoni sausage, Onions, Peppers, Chili peppers, Garlic',
+	'Fungi': 'Cheese, Tomato sauce, Mushrooms'
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
 	let list = document.getElementById('Pizzaliste');
-	Pizzaliste.forEach(function(pizza) {
+	PIZZALIST.forEach(function(pizza) {
 		let pizzaContainer = document.createElement('div');
-		pizzaContainer.className = 'card';
+		pizzaContainer.className = 'card responsive';
 		pizzaContainer.addEventListener("click", pizzaClicked)
 
 		let pizzaGridContainer = document.createElement('div');
@@ -42,13 +48,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function pizzaClicked(event) {
 	console.log('pizzaClicked');
+	console.log(event);
 	document.getElementById('pizzaDetails').removeAttribute('hidden');
+	let name = event.target.innerHTML;
+	document.getElementById('pizzaDetailsName').innerText = name;
+	document.getElementById('pizzaDetailsToppings').innerText = TOPPINGS[name];
+	document.getElementById('pizzaDetailsSize').innerText = 'Klein Mittel Groß';
 }
 
 function loginButtonClicked() {
 	console.log('loginButtonClicked');
 	let gridContainerMain = document.getElementById('grid-container-main');
-	if (gridContainerMain.getAttribute('style') == null || gridContainerMain.style.display == 'grid'){
+	if (gridContainerMain.getAttribute('style') == null || gridContainerMain.style.display == 'grid') {
 		gridContainerMain.style.display = 'none';
 	} else if (gridContainerMain.style.display == 'none') {
 		gridContainerMain.style.display = 'grid';
