170d7dc69cc9a82cb33d87a604511b914c9e8d85
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-12-13 09:21:51 +0100

    feat: docker compose logging

--- run.sh
+++ run.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
-echo "Down already running servives..."
+echo "Stopping already running services..."
+servicePath=./PizzaToGo/services
+cd "$servicePath" || exit 1
 
-docker-compose up -d ui delivery procurement production shop user
+shopt -s nullglob
+serviceArray=(*/)
+shopt -u nullglob 
+echo "Service directories found: ${serviceArray[@]}"
 
-# save logs to a file in the background
-docker-compose logs -f ui > ./PizzaToGo/services/ui/var/logs/ui.log 2>&1 &
-docker-compose logs -f delivery > ./PizzaToGo/services/delivery/var/logs/delivery.log 2>&1 &
-docker-compose logs -f procurement > ./PizzaToGo/services/procurement/var/logs/procurement.log 2>&1 &
-docker-compose logs -f production > ./PizzaToGo/services/production/var/logs/production.log 2>&1 &
-docker-compose logs -f shop > ./PizzaToGo/services/shop/var/logs/shop.log 2>&1 &
-docker-compose logs -f user > ./PizzaToGo/services/user/var/logs/user.log 
+docker compose down  # Stop currently running services if any
+
+docker compose up -d --build  # Bring up services
+
+for serviceDir in "${serviceArray[@]}"; do
+    (
+
+        serviceName="${serviceDir%/}"
+        cd $serviceName
+        # Check if dockerDB exists in the directory and if so, get the logs for the database service
+        if ls -d ./* | grep 'dockerDB'; then
+            docker compose logs -f "${serviceName}_database" > "./var/logs/${serviceName}_database.log" 2>&1 &
+        fi
+
+        # Get logs for the current service
+        docker compose logs -f "$serviceName" > "./var/logs/${serviceName}.log" 2>&1 &
+        cd ..
+    ) &
+done
+
+# Wait for all background processes to complete before continuing
+wait
+
+docker compose up --build  # Bring up services again (if needed)
+
+# serviceArr = ($services)
+# echo $serviceArr
+# docker-compose up -d $services --build
+
+# # save logs to a file in the background
+
+# logConsumer =  
+# docker-compose logs -f ui > ./PizzaToGo/services/ui/var/logs/ui.log 2>&1 &
+# docker-compose logs -f delivery > ./PizzaToGo/services/delivery/var/logs/delivery.log 2>&1 &
+# docker-compose logs -f procurement > ./PizzaToGo/services/procurement/var/logs/procurement.log 2>&1 &
+# docker-compose logs -f production > ./PizzaToGo/services/production/var/logs/production.log 2>&1 &
+# docker-compose logs -f shop > ./PizzaToGo/services/shop/var/logs/shop.log 2>&1 &
+# docker-compose logs -f user > ./PizzaToGo/services/user/var/logs/user.log 2>&1 &
 
 
-# reattach to the container to be able to do control-c
-# it will not rerun the container, just reattach
-docker-compose up ui delivery production procurement user shop
-
-# imagePath=./PizzaToGo/services
-# logPath=var/logs/
-# cd $imagePath
-# echo "Pwd: $(pwd)"
-#  shopt -s nullglob
-#  serviceArray=(*/)
-#  shopt -u nullglob 
+# docker-compose up $services --build
 
 
-
-# composeServicePayload=$(echo "${serviceArray[*]}" | sed 's/\// /g')
-# echo "Payload: $composeServicePayload"
-
-# echo (read -r -a myArray <<< "$composeServicePayload")
-
-# docker compose down   
-# echo "Start project with docker compose..."
-# echo "Rebuild containers..."
-
-
-# docker compose up $composeServicePayload --build
-
- $SHELL
+$SHELL
