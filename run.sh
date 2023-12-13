466178b055b9802fb7e4a9a667490db6a44f8a0c
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-12-13 09:22:55 +0100

    fix: cleanup

--- run.sh
+++ run.sh
@@ -22,33 +22,15 @@ for serviceDir in "${serviceArray[@]}"; do
             docker compose logs -f "${serviceName}_database" > "./var/logs/${serviceName}_database.log" 2>&1 &
         fi
 
-        # Get logs for the current service
         docker compose logs -f "$serviceName" > "./var/logs/${serviceName}.log" 2>&1 &
         cd ..
     ) &
 done
 
-# Wait for all background processes to complete before continuing
+
 wait
 
 docker compose up --build  # Bring up services again (if needed)
-
-# serviceArr = ($services)
-# echo $serviceArr
-# docker-compose up -d $services --build
-
-# # save logs to a file in the background
-
-# logConsumer =  
-# docker-compose logs -f ui > ./PizzaToGo/services/ui/var/logs/ui.log 2>&1 &
-# docker-compose logs -f delivery > ./PizzaToGo/services/delivery/var/logs/delivery.log 2>&1 &
-# docker-compose logs -f procurement > ./PizzaToGo/services/procurement/var/logs/procurement.log 2>&1 &
-# docker-compose logs -f production > ./PizzaToGo/services/production/var/logs/production.log 2>&1 &
-# docker-compose logs -f shop > ./PizzaToGo/services/shop/var/logs/shop.log 2>&1 &
-# docker-compose logs -f user > ./PizzaToGo/services/user/var/logs/user.log 2>&1 &
-
-
-# docker-compose up $services --build
 
 
 $SHELL
