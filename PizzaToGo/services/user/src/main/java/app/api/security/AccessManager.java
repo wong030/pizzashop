49fb86cfc38999c1d29f1281eaba7b14547de5a5
JanLudwig <jalu1002@stud.hs-kl.de>
2023-12-13 10:17:47 +0100

    feat: implement AccessManager class

--- PizzaToGo/services/user/src/main/java/app/api/security/AccessManager.java
+++ PizzaToGo/services/user/src/main/java/app/api/security/AccessManager.java
@@ -1,5 +1,49 @@
 package app.api.security;
 
-public class AccessManager {
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
+import javax.inject.Singleton;
+
+@Singleton
+public class AccessManager {
+	private Map<UUID, String> logins = new ConcurrentHashMap<>();
+	
+	public AccessManager() {
+		super();
+	}
+	
+	public boolean isLoggedIn(UUID loginID) {
+		return this.logins.containsKey(loginID);
+	}
+	
+	public Optional<String> getLoginName(UUID loginID){
+		String loginname = this.logins.get(loginID);
+		if(loginname != null) {
+			return Optional.of(loginname);
+		}else {
+			return Optional.empty();
+		}		
+	}
+	
+	public UUID login(String loginname) {
+		if(this.logins.containsValue(loginname)) {
+			RuntimeException exce = new RuntimeException("ERROR: User is already logged in");
+			throw exce;
+		}
+		UUID uuid = UUID.randomUUID();
+		this.logins.put(uuid, loginname);
+		return uuid;
+	}
+	
+	public void logout(UUID loginID) {
+		if(this.logins.containsKey(loginID) == false) {
+			RuntimeException exce = new RuntimeException("ERROR: User was not logged in");
+			throw exce;
+		}
+		this.logins.remove(loginID);
+		return;		
+	}
 }
